fc24c42d7ec712d03fc0602dd4d88c2e2952f1af
w0lf-46 <lucaspsrrio@gmail.com>
2024-07-02 07:20:30 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+console.log('running');
+
+var express = require('express');      
+
+var app = express();
+
+app.use(express.static(__dirname + '/public')); 
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.listen(3000);
+
+console.log('Listening on port 3000');
+
